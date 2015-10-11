ae89fcc9b324155d1ed29523c7bdf3fc4183c500
starmallc <mticciati@gmail.com>
2015-10-11 11:55:21 -0700

    testing incoming

--- routes/message-routes.js
+++ routes/message-routes.js
@@ -26,7 +26,69 @@ var Message = mongoose.model('Message');
 var router =  express.Router();
 
 // Twilio SMS webhook route
-router.route('/incoming').get(incoming.webhook);
+router.route('/incoming')
+	
+	.get(function(req, res) {
+
+		var phone = req.body.From;
+	    var responseMessage;
+	    console.log('response from client: ', phone);
+	    // Process any message the user sent to us
+	    function processMessage(phone) {
+	        // get the text message command sent by the user
+	        var msg = request.body.Body || '';
+	        msg = msg.toLowerCase().trim();
+
+	        // Conditional logic to do different things based on the command from
+	        // the user
+	        if (msg === 'yes' || msg === 'no') {
+	            // If the user has elected to subscribe for messages, flip the bit
+	            // and indicate that they have done so.
+	            /*
+	            subscriber.subscribed = msg === 'subscribe';
+	            subscriber.save(function(err) {
+	                if (err)
+	                    return respond('We could not subscribe you - please try '
+	                        + 'again.');
+
+	                // Otherwise, our subscription has been updated
+	                var responseMessage = 'You are now subscribed for updates.';
+	                if (!subscriber.subscribed)
+	                    responseMessage = 'You have unsubscribed. Text "subscribe"'
+	                        + ' to start receiving updates again.';
+	            
+	                respond(responseMessage);
+	            });*/
+	            
+	            if (msg === 'yes') {
+	                responseMessage = 'word.';
+	            }
+	            if (msg === 'no') {
+	                responseMessage = 'whaaaat?';
+	            }
+	            //respond(responseMessage);
+	        } else {
+	            // If we don't recognize the command, text back with the list of
+	            // available commands
+	            responseMessage = 'Come on now homie, available commands are: yes or no';
+
+	            
+	        }
+	        respond(responseMessage);
+	        console.log('attempting to respond with: ', responseMessage);
+	    }
+
+	    // Set Content-Type response header and render XML (TwiML) response in a 
+	    // Jade template - sends a text message back to user
+	    function respond(message) {
+	        console.log('redirecting to twiml to respond with: ', message);
+	        response.type('text/xml');
+	        response.render('twiml', {
+	            message: message
+	        });
+	    }
+
+	});
 
 router.route('/')
 	
