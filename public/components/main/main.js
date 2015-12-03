5a249ca7c59cb324a9d6f1b513429a8ff1989f69
mticciati <matthewticciati@gmail.com>
2015-12-03 15:08:48 -0800

    socket now sending live update from incoming, testing applying update to view

--- public/components/main/main.js
+++ public/components/main/main.js
@@ -155,8 +155,7 @@ angular.module('main', ['ngResource'])
 
 	socket.on('statusUpdate', function(data) {
 
-		console.log(data.status);
-		alert(data.status);
+		console.log('DATA: ', data);
 
 	});
 	
